1f23be80348225dfcba60299a3ed5e7161cb20ca
austin030606 <austin030606@gmail.com>
2022-06-14 00:59:56 +0800

    feat: move some functions to this file

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 from typing import Dict
 import pretty_midi
 import numpy as np
+import glob
 
 def midi_to_piano_roll(filename, version=1):
     def my_get_piano_roll(self, fs=100, times=None,
@@ -82,7 +83,11 @@ def midi_to_piano_roll(filename, version=1):
         else :
             tem = np.hstack((piano_roll, np.zeros((128, max_col - col))))
             drum_instruments += tem
-
+    tmp = []
+    for i in range(not_drum_instruments.shape[0]):   
+        tmp.append(not_drum_instruments[i])
+    tmp.append(drum_instruments[59])
+    not_drum_instruments = np.array(tmp)
     # version 2 (row: time, col: instrument, changed to zeros and ones)
     if version == 2:
         drum_instruments = np.where(drum_instruments > 0, 1, 0)
@@ -90,3 +95,80 @@ def midi_to_piano_roll(filename, version=1):
         not_drum_instruments = not_drum_instruments.transpose()
         
     return not_drum_instruments, drum_instruments
+
+def prepare(path):
+    X = []
+    Y = []
+    filelist = glob.glob(path + '/*.mid')
+    for file in filelist:
+        notdrum, drum = midi_to_piano_roll(file, version=2)
+        roll_length = drum.shape[0]
+        drum = drum.transpose()
+        tmp = []
+        for i in range(drum.shape[0]):
+            if i == 36 or i == 38:
+                tmp.append(drum[i])
+        drum = np.array(tmp)
+        drum = drum.transpose()
+
+        i = 0
+        while i < (roll_length // 512):
+            X.append(notdrum[i * 512 : (i + 1) * 512])
+            tmp = []
+            for row in drum[i * 512 : (i + 1) * 512]:
+                for j in row:
+                    tmp.append(j)
+            Y.append(tmp)
+            i += 1
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+def piano_roll_to_instrument(piano_roll, fs=100, program=0):
+    '''Convert a Piano Roll array into a PrettyMidi object
+     with a single instrument.
+    Parameters
+    ----------
+    piano_roll : np.ndarray, shape=(128,frames), dtype=int
+        Piano roll of one instrument
+    fs : int
+        Sampling frequency of the columns, i.e. each column is spaced apart
+        by ``1./fs`` seconds.
+    program : int
+        The program number of the instrument.
+    Returns
+    -------
+    midi_object : pretty_midi.PrettyMIDI
+        A pretty_midi.PrettyMIDI class instance describing
+        the piano roll.
+    '''
+    notes, frames = piano_roll.shape
+    instrument = pretty_midi.Instrument(program=program)
+
+    # pad 1 column of zeros so we can acknowledge inital and ending events
+    piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
+
+    # use changes in velocities to find note on / note off events
+    velocity_changes = np.nonzero(np.diff(piano_roll).T)
+
+    # keep track on velocities and note on times
+    prev_velocities = np.zeros(notes, dtype=int)
+    note_on_time = np.zeros(notes)
+
+    for time, note in zip(*velocity_changes):
+        # use time + 1 because of padding above
+        velocity = piano_roll[note, time + 1]
+        time = time / fs
+        if velocity > 0:
+            if prev_velocities[note] == 0:
+                note_on_time[note] = time
+                prev_velocities[note] = velocity
+        else:
+            pm_note = pretty_midi.Note(
+                velocity=prev_velocities[note],
+                pitch=note,
+                start=note_on_time[note],
+                end=time)
+            instrument.notes.append(pm_note)
+            prev_velocities[note] = 0
+    return instrument
