2687f33dddd6a223b152de5a96c2681a3aa1feb2
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2022-06-16 17:26:53 +0800

    feat: add train_model.py

---
+++ train_model.py
@@ -0,0 +1,43 @@
+import dataset
+import numpy as np
+from tensorflow.keras import layers, models
+import pickle
+
+def CNN():
+    X, Y = dataset.prepare('test/midi/training')
+    X_val, Y_val = dataset.prepare('test/midi/validation')
+
+    model = models.Sequential()
+    model.add(layers.Conv1D(filters=32, kernel_size=32, activation='relu', input_shape=(X[0].shape[0], X[0].shape[1])))
+    model.add(layers.MaxPooling1D(pool_size=2))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='elu'))
+    model.add(layers.Dense(Y[0].shape[0], activation='relu'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    history = model.fit(X, Y, epochs=5, verbose=1, validation_data=(X_val, Y_val))
+    model.save("test/saved_model")
+    with open('test/1sttrainHistoryDict', 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
+
+def RNN():
+    X, Y = dataset.prepare('test/midi/smaller/training')
+    X_val, Y_val = dataset.prepare('test/midi/smaller/validation')
+
+    model = models.Sequential()
+    model.add(
+        layers.LSTM(
+        512,
+        input_shape=(X[0].shape[0], X[0].shape[1]),
+        return_sequences=True
+    ))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.LSTM(512))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(Y[0].shape[0], activation='relu'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    history = model.fit(X, Y, epochs=5, verbose=1, validation_data=(X_val, Y_val))
+    model.save("test/saved_model")
+    with open('test/1sttrainHistoryDict', 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
